0d6e361b9641db42833e5f5e3a8bd32615c46532
Oluwafemi Olayemi <3621041+Oluwafemi-Olayemi@users.noreply.github.com>
2022-04-24 20:59:22 +0100

    Initial Commit

---
+++ src/main/java/com/example/droneservice/DroneserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.droneservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DroneserviceApplication.class, args);
+    }
+
+}
